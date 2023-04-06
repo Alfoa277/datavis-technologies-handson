9848329e46267dc097aa51898e85f6d3f21ed79c
Alfoa277 <alarumbe.student@gmail.com>
2023-04-06 21:38:19 +0200

    More exercises

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,5 @@
+<script>
+    const thingies = {name:"Alfonso",things:["books",'cats','chocolates']};
+</script>
+
+My name is {thingies.name} and these are some of my favourite things: {thingies.things}
---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,12 @@
+<script>
+
+    const thisRadius = 1.67;
+
+    function circum(radius){
+        const solution = 2*3.141592*radius;
+        return solution.toFixed(2);
+    }
+
+</script>
+
+The circumference of a circle with radius {thisRadius}m is {circum(thisRadius)}m
